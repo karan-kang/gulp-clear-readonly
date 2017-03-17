72e51d487df49cb90c80045c96a47fd4342eeae6
Karanvir Kang <Karankang007@gmail.com>
2017-03-17 11:31:40 -0400

    Updated logic to do proper module export
    Add options object to optionally turn off debugging.

--- index.js
+++ index.js
@@ -1,65 +1,86 @@
-'use strict'
-
 var gutil = require('gulp-util');
+var cp = require('child_process');
+var moduleName = 'gulp-clear-readonly';
+var debug = gutil.log;
 
-module.exports = function(){
-    var cp = require('child_process');
-    return {
-        clear: clearReadOnly
-    };
+/**
+ * Clear readOnly flag from all files and sub folders.
+ * @name clearReadOnly
+ * @param {string} path - Folder from which read-only flag needs to be cleared
+ * @param {function} callback - Callback function to invoke
+ * @param {Object} opts - Plugin options { isFile, debug }
+ */
+function clearReadOnly(path, callback, providedOptions) {
+	var options = getOptions(providedOptions);
+	debug = options.debug ? gutil.log : function (){};
+	debug(moduleName, 'Clearing read-only flag for path: ' + path + '...');
 
-    /**
-     * Clear readOnly flag from all files and sub folders.
-     * @name clearReadOnly
-     * @param {string} path - Folder from which read-only flag needs to be cleared
-     * @param {function} callback - Callback function to invoke
-     * @param {boolean} isFile - Whether the path is a file. Default: False
-     */
-    function clearReadOnly(path, callback, isFile) {
-        gutil.log('Clearing read-only flag for path:' + path + '...');
-        var clearReadOnlyCommand = getCommand(path, !isFile);
+	var clearReadOnlyCommand = getCommand(path, !options.isFile);
+	cp.exec(clearReadOnlyCommand, function (error) {
+		// We only care about errors for this command
+		if (error) {
+			// Handle failure scenario
+			debug(moduleName, error);
+			throw new gutil.PluginError(moduleName, 'Failed to clear read only flag.');
+		} else {
+			// Mark this asynchronous task as completed
+			debug(moduleName, 'Cleared read-only flag for path: ' + path);
 
-        cp.exec(clearReadOnlyCommand, function(error) {
-            // We only care about errors for this command
-            if (error) {
-                // Handle failure scenario
-                gutil.log(error);
-                throw new gutil.PluginError('clear:readonly',
-                    'Failed to clear read only flag.');
-            } else {
-                // Mark this asynchronous task as completed
-                gutil.log('Cleared read-only flag for path:' + path);
-                // Invoke callback
-                if (callback) {
-                    callback();
-                }
-            }
-        });
-    }
+			// Invoke callback
+			if (callback) {
+				callback();
+			}
+		}
+	});
+}
 
-    /**
-     * Get clear command
-     * @name getCommand
-     * @param {string} path - Path from which read-only flag needs to be cleared
-     * @param {boolean} isFile - Whether the specified path is a file. Default: Path is a folder
-     */
-    function getCommand(path, isFile) {
-        if (/^win/.test(process.platform)) {
-            // Is Windows
-            if (isFile) {
-                command = 'attrib -r "' + path + '\\*.*" /s';
-            } else {
-                command = 'attrib -r "' + path + '"';
-            }
-        } else {
-            // Is Linux or Unix
-            if (isFile) {
-                // Check if file exists before executing command
-                command = '[ -f "' + path + '" ] && chmod 777 "' + path + '" -f';
-            } else {
-                // Check if folder exists before executing command
-                command = '[ -d "' + path + '" ] && chmod 777 "' + path + '" -f -R';
-            }
-        }
-    }
-};
+/**
+ * Get clear read-only flag command
+ * @name getCommand
+ * @param {string} path - Path from which read-only flag needs to be cleared
+ * @param {boolean} isFile - Whether the specified path is a file. Default: Path is a folder
+ * @return {string} Clear read-only flag command
+ */
+function getCommand(path, isFile) {
+	var command;
+	if (/^win/.test(process.platform)) {
+		debug(moduleName, 'Detected platform: Windows');
+		
+		// Is Windows
+		if (isFile) {
+			command = 'attrib -r "' + path + '\\*.*" /s';
+		} else {
+			command = 'attrib -r "' + path + '"';
+		}
+	} else {
+		debug(moduleName, 'Detected platform: Non-Windows');
+		
+		// Is Linux or Unix
+		if (isFile) {
+			// Check if file exists before executing command
+			command = '[ -f "' + path + '" ] && chmod 777 "' + path + '" -f';
+		} else {
+			// Check if folder exists before executing command
+			command = '[ -d "' + path + '" ] && chmod 777 "' + path + '" -f -R';
+		}
+	}
+	
+	debug(moduleName, 'Command: ' + command);
+	return command;
+}
+
+/**
+ * Merge provided options with default options
+ * @name getCommand
+ * @param {object} options - Plugin options
+ * @return {object} Merged options
+ */
+function getOptions(options){
+	options = options || {};
+	options.isFile =  'isFile' in options ? options.isFile : false;
+	options.debug = 'debug' in options ? options.debug : false;
+	return options;
+}
+
+// Export this module
+module.exports = clearReadOnly;
